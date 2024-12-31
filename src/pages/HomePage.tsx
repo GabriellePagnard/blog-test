eefb1b6959a7e2a5122b8c3124a581bdd2ea675b
Gabrielle Pagnard <gpagnard@gmail.com>
2025-01-01 00:43:19 +0100

    Add router and basic pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Bienvenue sur le Blog</h1>
+        <p>Voici la page d'accueil.</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+      <div>
+        <h1>404</h1>
+        <p>Page non trouvée.</p>
+      </div>
+    );
+  };
+  
+  export default NotFoundPage;
