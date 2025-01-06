91c2151b67a56b765523942de842909fa4419811
Gabrielle Pagnard <gpagnard@gmail.com>
2025-01-06 08:30:18 +0100

    Add router and basic pages

---
+++ src/pages/ArticlesPage.tsx
@@ -0,0 +1,10 @@
+const ArticlesPage = () => {
+    return (
+      <div>
+        <h1>Tous les Articles</h1>
+        <p>Liste des articles.</p>
+      </div>
+    );
+  };
+  
+  export default ArticlesPage;
